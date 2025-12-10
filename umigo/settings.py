ccaf07045098c9816ef3659755a98157e616f3f8
Mauro-Suesca <msuesca@unal.edu.co>
2025-12-09 23:17:20 -0500

    Updated settings.py so production can detect the css files

--- umigo/settings.py
+++ umigo/settings.py
@@ -145,8 +145,7 @@ STATIC_ROOT = BASE_DIR / 'assets'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-if DEBUG:
-    STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 
 
